65aa0dd2aa359b48e679bdee36eef858f2b2242e
DedlySpyder <pooreboy464@gmail.com>
2020-10-05 23:10:09 -0400

    0.3.1 Fixed error with 0.1.0 migration due to new naming scheme

--- migrations/Generic_Logistic_Chest_0.1.0.lua
+++ migrations/Generic_Logistic_Chest_0.1.0.lua
@@ -1,7 +1,7 @@
 for _, force in pairs(game.forces) do 
 	force.reset_technologies()
 	
-	if force.technologies["logistic-system"].researched then
+	if force.technologies["logistic-system"].researched and force.recipes["generic-logistic-chest"] then
 		force.recipes["generic-logistic-chest"].enabled = true
 	end
 end
